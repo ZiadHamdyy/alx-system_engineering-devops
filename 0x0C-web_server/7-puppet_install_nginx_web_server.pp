84989ca110f582f3bb2fa9a368ed6e62739226c9
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-01-04 01:05:57 +0200

    task 5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,80 +1,38 @@
-# 7-puppet_install_nginx_web_server.pp
-
 # Install Nginx package
 package { 'nginx':
-  ensure => 'installed',
+  ensure => present,
+}
+
+# Configure Nginx site
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  replace => true,
+  content => "# Nginx default site configuration\n
+              server {\n
+                listen 80 default_server;\n
+                listen [::]:80 default_server;\n\n
+                root /var/www/html;\n
+                index index.html index.htm index.nginx-debian.html;\n\n
+                server_name _;\n\n
+                location / {\n
+                  try_files $uri $uri/ =404;\n
+                }\n\n
+                location /redirect_me {\n
+                  return 301 http://$host/;\n
+                }\n
+              }",
+  notify => Service['nginx'],
 }
 
 # Ensure Nginx service is running and enabled
 service { 'nginx':
-  ensure => 'running',
-  enable => true,
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/nginx/sites-available/default'],
 }
 
-# Configure Nginx main configuration file
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  source  => 'puppet:///modules/nginx/nginx.conf',
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Create the Hello World index file
+# Create Hello World index.html file
 file { '/var/www/html/index.html':
-  ensure  => file,
+  ensure  => present,
   content => 'Hello World!',
-  require => Package['nginx'],
-  notify  => Service['nginx'],
 }
-
-# Configure Nginx site configuration
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => template('nginx/default.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Enable the site by creating a symbolic link
-file { '/etc/nginx/sites-enabled/default':
-  ensure  => link,
-  target  => '/etc/nginx/sites-available/default',
-  require => File['/etc/nginx/sites-available/default'],
-  notify  => Service['nginx'],
-}
-
-# Create a custom redirect configuration for /redirect_me
-file { '/etc/nginx/sites-available/redirect_me':
-  ensure  => file,
-  content => template('nginx/redirect_me.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Enable the redirect site by creating a symbolic link
-file { '/etc/nginx/sites-enabled/redirect_me':
-  ensure  => link,
-  target  => '/etc/nginx/sites-available/redirect_me',
-  require => File['/etc/nginx/sites-available/redirect_me'],
-  notify  => Service['nginx'],
-}
-
-# Custom facts for Nginx
-file { '/etc/facter/facts.d/nginx_facts.txt':
-  ensure  => file,
-  content => 'nginx_version=1.18.0',
-}
-
-# Notify Puppet to reload Nginx when configuration changes
-exec { 'nginx-reload':
-  command     => 'systemctl reload nginx',
-  refreshonly => true,
-  subscribe   => [
-    File['/etc/nginx/nginx.conf'],
-    File['/etc/nginx/sites-available/default'],
-    File['/etc/nginx/sites-available/redirect_me'],
-  ],
-}
-
-# End of Puppet manifest
-
